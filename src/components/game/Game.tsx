d5750ad7957c73405e39ac9d41b5f3c615f40449
octo <v.minaev@qiwi.com>
2023-11-12 13:13:59 +0300

    interact

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import interact from "interactjs";
+
 import "./Engine";
 
 export const Game = () => {
-  return <div id="game"></div>;
+  interact("#game").gesturable({
+    onstart: () => {},
+    onend: () => {},
+  });
+
+  return <div id="game">test</div>;
 };
